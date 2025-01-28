e90775a98e91c2309cfced62315c8c09d0da0699
Aman Abuelayyan <153456442+safety-crew@users.noreply.github.com>
2025-01-28 18:59:31 +0200

    Time: 5ms (54.06%), Space: 42.65 MB (98.95%)

---
+++ 242. Valid Anagram/242-Valid-Anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // 1. Check length for each of s, and t.
+        // 2. Use an array that represents all English alphabet (a -> z) [length: 26 fixed].
+        // 3. In that array.. we check each string (s and t) for the frequency of each
+        //    letter. As we move through s, we increment that index, and as we move through
+        //    t, we decrement that index. The array should have all zeros if the two strings
+        //    are anagrams.
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charsNum = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            charsNum[s.charAt(i) - 'a']++;
+            charsNum[t.charAt(i) - 'a']--;
+        }
+
+        for(int num: charsNum) {
+            if(num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
