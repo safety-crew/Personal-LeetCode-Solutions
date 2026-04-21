9352d09a7090a96609a65243da28e331df032fc9
Aman Abuelayyan <153456442+safety-crew@users.noreply.github.com>
2026-04-21 20:28:47 +0300

    Time: 10ms (58.30%); Space: 45.96MB (90.68%)

---
+++ 424. Longest Repeating Character Replacement/424-Longest-Repeating-Character-Replacement.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] occurence = new int[26];
+        int left = 0, right;
+        int answer = 0;
+        int maxOccurence = 0;
+
+        for (right = 0; right < s.length(); right++) {
+            maxOccurence = Math.max(maxOccurence, ++occurence[s.charAt(right) - 'A']);
+
+            if (right - left + 1 - maxOccurence > k) {
+                occurence[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            answer = Math.max(answer, right - left + 1);
+        }
+
+        return answer;
+    }
+}
